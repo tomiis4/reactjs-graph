dbd12ca30a153f02a97927a046d3a3c349533072
tomiis4 <87276646+tomiis4@users.noreply.github.com>
2022-05-08 20:35:42 +0200

    Delete getItemWidth.jsx

--- src/Components/components/getItemWidth.jsx
+++
@@ -1,7 +0,0 @@
-const getItemWidth = (itemWidth, itemHeight) => {
-    let mainWidth = Math.sqrt(itemWidth * itemWidth + itemHeight * itemHeight)
-    
-    return mainWidth
-}
-
-export default getItemWidth
